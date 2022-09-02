3357554d0445ba28a3d30d02704ab9e90c285f20
jaimon-repo <ijaimonjames@gmail.com>
2022-09-02 22:16:26 +0530

    seeder added

--- app/Http/Controllers/AngularController.php
+++ app/Http/Controllers/AngularController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Library_list;
 
 class AngularController extends Controller
 {
     public function index()
     {
         return view('angular');
+    }
+
+    public function getLibraryList()
+    {
+        return response()->json(Library_list::all(), 200);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Book_list;
+use App\Models\Library_list;
+use App\Models\Book_keep;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            "username" => "admin",
+            "email" => "admin@library-app.test",
+            "password" => bcrypt("123456"),
+            "super_user" => 1,
+        ]);
+        Book_list::create([
+            "name" => "Sample",
+            "added_by" => "1",
+            "status" => "1",
+        ]);
+        Book_list::create([
+            "name" => "Sample2",
+            "added_by" => "1",
+            "status" => "1",
+        ]);
+        Book_list::create([
+            "name" => "Sample3",
+            "added_by" => "1",
+            "status" => "1",
+        ]);
+        Library_list::create([
+            "name" => "Test Library",
+            "address" => "Location1 ,Denver",
+            "added_by" => 1,
+            "book_collection" => json_encode(1)
+        ]);
+        Library_list::create([
+            "name" => "Test2 Library2",
+            "address" => "Location2 , Newyork",
+            "added_by" => 1,
+            "book_collection" => json_encode(2, 3)
+        ]);
+        Book_keep::create([
+            "book_id" => 1,
+            "library_id" => 1,
+            "status"  => "IN",
+            "return_dt" => "2022-09-25"
+        ]);
+        Book_keep::create([
+            "book_id" => 2,
+            "library_id" => 2,
+            "status"  => "OUT",
+            "return_dt" => "2022-09-27"
+        ]);
     }
 }
